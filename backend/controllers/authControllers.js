5d10d5aff37c04c5cf737d529292e033ba9706d8
asmagtr <gtrasma@gmail.com>
2024-10-23 23:58:45 +0100

    added login function

--- backend/controllers/authControllers.js
+++ backend/controllers/authControllers.js
@@ -2,7 +2,7 @@ const userModel = require('../models/user');
 const { hashPassword, generateToken } = require('../lib/auth'); 
 
 
-
+//function to create account
 async function signUp(req, res) {
 
 
@@ -58,3 +58,64 @@ async function signUp(req, res) {
    
   }
   
+
+//function to login
+async function login(req,res){
+
+    const {emailOrUsername,password}=req.body;
+    if(!emailOrUsername){
+        return res.status(400).json({error:true,
+            message:"ENter Email or Username"});
+    }
+    if(!password){
+        return res.status(400).json({error:true,
+            message:"Password is required"});
+    }
+
+
+    try{
+           //first we look if the provided body has an email 
+    let userInfo=await userModel.findOne({email:emailOrUsername});
+   //if not than we look for an account with that username
+   if(!userInfo){
+    userInfo =await userModel.findOne({username:emailOrUsername});
+   }
+
+   if(!userInfo){
+    return res.status(404).json({
+        error:true,
+        message:"User Not Found",
+    });
+   }
+
+  
+    const isValidPassword = await hash.comparePassword(password, userInfo.password);
+
+
+    if(!isValidPassword){
+        return res.status(400).json({
+            error:true,
+            message:"Invalid Credentials"});
+    }
+
+    if(isValidPassword){
+        const accessToken=generateToken(userInfo._id);
+
+        return res.json({
+            error:false,
+            message:"Login Successful",
+            accessToken,
+        });
+
+    }
+
+    }catch(error){
+          
+        return res.status(500).json({
+            error:true,
+            message:"server error"
+        });
+    }
+ 
+
+}
