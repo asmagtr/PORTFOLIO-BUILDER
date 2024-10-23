54d6d7692f8c610fb3a306984cd8eb7fb538326e
asmagtr <gtrasma@gmail.com>
2024-10-23 23:19:57 +0100

    set up express server+ changed connect to db function

---
+++ backend/index.js
@@ -0,0 +1,33 @@
+const express=require("express");
+const cors=require("cors");
+require('dotenv').config();
+const connectToDB=require("./lib/dbConnect")
+
+
+
+const app = express();
+app.use(cors());
+app.use(cors({
+    origin: '*'
+  }));
+  app.use(express.json());
+
+ 
+
+
+
+
+
+
+
+  
+const PORT = process.env.PORT || 3000;
+const MONGO_URI = process.env.MONGO_URI;
+app.listen(PORT,async () => {
+    console.log(`Server is running on port ${PORT}`);
+    try{
+        await connectToDB(MONGO_URI);
+    }catch{
+        console.log("error in connection to database ");
+    }
+  });
--- backend/lib/dbConnect.js
+++ backend/lib/dbConnect.js
@@ -1,25 +1,16 @@
-const mongoose = require('mongoose');
-
-
- const connectToDB=async()=>{
-
-    if (mongoose.connection.readyState === 1) {
-        console.log("the db is already connected");
-        return;
-    }
-    const MONGODB_URI=process.env.MONGODB_URI
-
-
+const mongoose=require("mongoose");
+const connectToDB = async(url)=>{
     try{
-        await mongoose.connect(MONGODB_URI,{
-            dbName:"portfolioBuilder",
-        });
-        console.log("db connected succesfully")
-    }catch(e){
-        console.log(e);
+        await mongoose.connect(url, {
+            dbName: "portfolioBuilder",
+          });
+    
+        console.log("connected to the databse");
+    }catch(error){
+        console.log(error)
     }
 
-    
+
 }
 
-module.exports= connectToDB;
+module.exports=connectToDB;
