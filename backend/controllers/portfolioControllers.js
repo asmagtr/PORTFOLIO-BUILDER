a09ff46422dab255e261cdeb7b820a1ae5d5a468
asmagtr <gtrasma@gmail.com>
2024-10-26 13:18:53 +0100

    created routes and controllers file for portfolio

---
+++ backend/controllers/portfolioControllers.js
@@ -0,0 +1,14 @@
+const Portfolio=require("../models/portfolio");
+
+//function to create a porfolio for a user
+async function createPorfolio(req,res){
+    
+}
+
+
+
+
+
+module.exports={
+    createPorfolio,
+}
