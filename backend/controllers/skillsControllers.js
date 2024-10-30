0f896667e2fe10bb651af60a81f8e80d18759ade
asmagtr <gtrasma@gmail.com>
2024-10-30 23:26:05 +0100

    +add skill to fill the db

--- backend/controllers/skillsControllers.js
+++ backend/controllers/skillsControllers.js
@@ -14,3 +14,36 @@ async function getAllSkills(req,res){
             errorMessage: 'Failed to fetch skills' });
       }
 }
+
+
+async function addSkill(req,res){
+    try {
+        const { name, svgUrl } = req.body;
+        if(!name || !svgUrl){
+            return res.status(400).json({ message: 'Please provide both skill name and svgUrl' });
+        }
+
+        // Check if the name or svgUrl already exists
+        const existingSkill = await skillModel.findOne({ $or: [{ name }, { svgUrl }] });
+
+        if (existingSkill) {
+            return res.status(400).json({ message: 'Skill with this name or svgUrl already exists.' });
+        }
+
+        // Create a new skill if it doesn't exist
+        const newSkill = new skillModel({ name, svgUrl });
+        await newSkill.save();
+
+        return res.status(201).json({ message: 'Skill added successfully!', skill: newSkill });
+    } catch (error) {
+        console.error(error);
+        return res.status(500).json({ message: 'Server error', error: error.message });
+    }
+
+
+}
+
+module.exports={
+    getAllSkills,
+    addSkill
+}
--- backend/routes/skillsRoutes.js
+++ backend/routes/skillsRoutes.js
@@ -1,9 +1,12 @@
 const express = require('express');
 const router = express.Router();
 const authenticateUser=require("../middlewares/authToken");
+const {getAllSkills,addSkill}=require("../controllers/skillsControllers")
 
 
 
 router.get("/",authenticateUser,getAllSkills)
+
+router.post("/",)
 
 module.exports = router;
